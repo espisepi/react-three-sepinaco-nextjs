1f03a745048c6882abcb9df7732f4d0fd651434e
espisepi <sepinaco@gmail.com>
2025-09-20 23:26:54 +0200

    added stars in DarkWheelScene

--- src/features/wheel-of-fortune/scenes/DarkWheelScene.tsx
+++ src/features/wheel-of-fortune/scenes/DarkWheelScene.tsx
@@ -1,13 +1,28 @@
 import React, { memo } from 'react'
+import { Stars } from '@react-three/drei'
 import { WheelScene } from '@/features/wheel-of-fortune/components/canvas/WheelScene'
 import { WheelSceneProps } from '@/types/scene-manager'
 
 /**
- * Escena oscura de la ruleta - Por ahora usa la escena original
+ * Escena oscura de la ruleta con efecto de estrellas de fondo
  * TODO: Implementar efectos de neón y ambiente misterioso
  */
 export const DarkWheelScene = memo((props: WheelSceneProps) => {
-    return <WheelScene {...props} />
+  return <>
+    {/* Efecto de estrellas de fondo */}
+    <Stars
+      radius={100}
+      depth={50}
+      count={5000}
+      factor={4}
+      saturation={0}
+      fade={true}
+      speed={1}
+    />
+
+    {/* Escena original de la ruleta */}
+    <WheelScene {...props} />
+  </>
 })
 
 DarkWheelScene.displayName = 'DarkWheelScene'
